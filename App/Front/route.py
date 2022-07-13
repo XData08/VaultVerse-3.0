fd0861268e8b3a882b8b2781f7a5db4b2a5ebbae
Lirae Que Data <lqdata51@gmail.com>
2022-07-14 00:41:57 +0800

    Increment #1 : Navigation, pages, modals, and card

--- App/Front/route.py
+++ App/Front/route.py
@@ -7,3 +7,18 @@ Front : Blueprint = Blueprint("Front", __name__)
 @Front.route("/")
 def IndexPage() -> str:
     return render_template("index.html")
+
+
+@Front.route("/services")
+def ServicesPage() -> str:
+    return render_template("services.html")
+
+
+@Front.route("/about")
+def AboutPage() -> str:
+    return  render_template("about.html")
+
+
+@Front.route("/<path:path>")
+def ErrorPage(path) -> str:
+    return render_template("error.html", error=True)
